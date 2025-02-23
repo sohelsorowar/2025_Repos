4ea756cde356105179e2e188b698e3576ac92f85
Sohel <sohelsorowar@gmail.com>
2025-02-23 12:07:14 +0600

    hello, syntax, version is committed

---
+++ python/syntax.py
@@ -0,0 +1,12 @@
+
+
+if 5>2:
+    print("Five is grater than two ")
+#Python will give you an error if you skip the indentation:
+
+if 5<2:
+    print("5<2","False")
+#this print will not show output because the statement is wrong.
+else:
+    print("Two is less than five")
+    
